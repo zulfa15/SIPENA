8d95ddd0eea82815a271eb4aafacac9be40b54c4
zulfa15 <istiqomahzulfanuraini123@gmail.com>
2025-09-21 14:33:41 +0700

    tambah lokasi

--- app/Http/Controllers/PresensiController.php
+++ app/Http/Controllers/PresensiController.php
@@ -9,4 +9,16 @@ class PresensiController extends Controller
     public function create(){
         return view('presensi.create');
     }
+
+
+    public function store(Request $request)
+    {
+        $nik = Auth::guard('karyawan')->user()->nik;
+        $tgl_presensi = date("Y-m-d");
+        $jam = date("H:i:s");
+        $lokasi = $request->lokasi;
+        $image = $request->image; 
+        $folderPath = ""
+
+    }
 }
--- resources/views/presensi/create.blade.php
+++ resources/views/presensi/create.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.presensi')
+
 @section('header')
     <!-- App Header -->
     <div class="appHeader bg-primary text-light">
@@ -12,65 +13,117 @@
     </div>
     <!-- * App Header -->
 
-<style>
-    /* Styling webcam */
-    .webcam-capture,
-    .webcam-capture video {
-        display: inline-block;
-        width: 100% !important;
-        margin: auto;
-        height: auto !important;
-        border-radius: 15px;
-    }
-</style>
+    <style>
+        /* Styling webcam */
+        .webcam-capture,
+        .webcam-capture video {
+            display: inline-block;
+            width: 100% !important;
+            margin: auto;
+            height: auto !important;
+            border-radius: 15px;
+        }
 
+        #map {
+            height: 200px;
+        }
+    </style>
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"/>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
 @endsection
 
 @section('content')
-<div class="row" style="margin-top: 70px">
-    <div class="col">
-        <input type="hidden" id="lokasi" class="form-control mb-3" placeholder="Koordinat lokasi">
-        <div class="webcam-capture"></div>
-    </div>    
-</div>
-<div class="row">
-    <div class="col">
-         <button id="takeabsen" class="btn btn-primary btn-block">
-            <ion-icon name="camera-outline"></ion-icon>
-            Absen Masuk</button>
-
+    <div class="row" style="margin-top: 70px">
+        <div class="col">
+            <input type="hidden" id="lokasi" class="form-control mb-3" placeholder="Koordinat lokasi">
+            <div class="webcam-capture"></div>
+        </div>
     </div>
-</div>
+
+    <div class="row">
+        <div class="col">
+            <button id="takeabsen" class="btn btn-primary btn-block">
+                <ion-icon name="camera-outline"></ion-icon>
+                Absen Masuk
+            </button>
+        </div>
+    </div>
+
+    <div class="row mt-2">
+        <div class="col">
+            <div id="map"></div>
+        </div>
+    </div>
 @endsection
 
 @push('myscript')
-<script>
-    // Setting webcam
-    Webcam.set({
-        height: 480,
-        width: 640,
-        image_format: 'jpeg',
-        jpeg_quality: 80
-    });
+    <script>
+        // Setting webcam
+        Webcam.set({
+            height: 480,
+            width: 640,
+            image_format: 'jpeg',
+            jpeg_quality: 80
+        });
+        Webcam.attach('.webcam-capture');
 
-    Webcam.attach('.webcam-capture');
+        // Ambil lokasi
+        const lokasi = document.getElementById('lokasi');
 
-    // Ambil lokasi
-    var lokasi = document.getElementById('lokasi');
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+        } else {
+            lokasi.value = "Geolocation tidak didukung browser ini";
+        }
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-    } else {
-        lokasi.value = "Geolocation tidak didukung browser ini";
-    }
+        function successCallback(position) {
+            // ambil latitude & longitude
+            lokasi.value = position.coords.latitude + "," + position.coords.longitude;
 
-    function successCallback(position) {
-        // ambil latitude & longitude
-        lokasi.value = position.coords.latitude + "," + position.coords.longitude;
-    }
+            // tampilkan peta
+            const map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 15);
 
-    function errorCallback(error) {
-        lokasi.value = "Tidak bisa mendapatkan lokasi: " + error.message;
-    }
-</script>
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
+
+            // marker
+            L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+
+            // lingkaran radius
+            L.circle([position.coords.latitude, position.coords.longitude], {
+                color: 'red',
+                fillColor: '#f03',
+                fillOpacity: 0.5,
+                radius: 100
+            }).addTo(map);
+        }
+
+        function errorCallback(error) {
+            lokasi.value = "Tidak bisa mendapatkan lokasi: " + error.message;
+        }
+
+        $("#takeabsen").click(function (e) {
+            Webcam.snap(function(uri) {
+                image = uri;
+            });
+            var lokasi = $('#lokasi').val();
+            $.ajax({
+            type: 'POST',
+            url: '/presensi/store',
+            data:{
+                _token: "{{ csrf_token }}",
+                image: image,
+                lokasi: lokasi
+            }
+            cache: false 
+            success: function(respond) {
+
+            }
+            });
+            
+        });
+    </script>
 @endpush
